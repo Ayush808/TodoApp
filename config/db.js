624fdb2f0ee92be63bd0813bafa879733c68ca2a
Ayush808 <mahajanayush808@gmail.com>
2020-03-19 13:23:12 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = config.get('MONGODBURI')
 
 const ConnectDB = async () => {
     try {
-        await mongoose.connect(db, {
+        await mongoose.connect(db || process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
